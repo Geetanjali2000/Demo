ed997237e3cce41072096f33cb8d8117140100ee
Geetanjali <-0geetanjalisahu2000@gmail.com>
2025-04-07 14:50:07 +0530

    remove commandLine Interface in Spingbootapplication file

--- src/main/java/com/example/demo/GraphqldemoApplication.java
+++ src/main/java/com/example/demo/GraphqldemoApplication.java
@@ -9,45 +9,12 @@ import com.example.demo.entities.Book;
 import com.example.demo.services.BookService;
 
 @SpringBootApplication
-public class GraphqldemoApplication implements CommandLineRunner{
+public class GraphqldemoApplication{
 
-	@Autowired
-	private BookService bookService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqldemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Book b1 = new Book();
-		b1.setTitle("Complete Reference");
-		b1.setName("Java");
-		b1.setPages(2000);
-		b1.setPrice(5000);
-		b1.setAuthor("ABC");
-		
-		Book b2 = new Book();
-		b2.setTitle("Complete Reference");
-		b2.setName("Java");
-		b2.setPages(2000);
-		b2.setPrice(5000);
-		b2.setAuthor("PQR");
-		
-		
-		Book b3 = new Book();
-		b3.setTitle("Complete Python");
-		b3.setName("Python");
-		b3.setPages(2000);
-		b3.setPrice(5000);
-		b3.setAuthor("XYZ");
-		
-		this.bookService.create(b1);
-		this.bookService.create(b2);
-		this.bookService.create(b3);
-	}
 	
-	
-
 }
